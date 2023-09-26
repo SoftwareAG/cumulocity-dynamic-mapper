fd25b128432d260a0e228d1ca7ce140c67d18d95
Michael Voigt <michael.voigt@softwareag.com>
2023-09-26 15:11:34 +0200

    add the dispatcher

---
+++ mqtt-mapping-service/src/main/java/mqtt/mapping/service/SimpleJsonDispatcher.java
@@ -0,0 +1,45 @@
+package mqtt.mapping.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.cumulocity.microservice.subscription.model.MicroserviceSubscriptionAddedEvent;
+import com.cumulocity.microservice.subscription.service.MicroserviceSubscriptionsService;
+import com.cumulocity.rest.representation.measurement.MeasurementRepresentation;
+import com.cumulocity.sdk.client.measurement.MeasurementApi;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class SimpleJsonDispatcher {
+	
+    @Autowired
+    private MicroserviceSubscriptionsService subscriptionsService;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Autowired
+	private MeasurementApi measurementApi;
+
+	private String tenant;
+	
+	
+    @EventListener
+    public void initialize(MicroserviceSubscriptionAddedEvent event) {
+        tenant = event.getCredentials().getTenant();
+    }
+	
+	public void createMeasurement(String json) {
+		try {
+			MeasurementRepresentation measurement = objectMapper.readValue(json, MeasurementRepresentation.class);
+			subscriptionsService.callForTenant(tenant, ()->measurementApi.create(measurement));
+		} catch (Exception e) {
+			log.error("error!", e);
+		}
+	}
+
+}
